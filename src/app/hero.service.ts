9d58033ba0f7f59666d956d7907ac94ae9ab2712
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2022-03-11 13:26:59 -0300

    add navigation and dashboard component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,10 +9,17 @@ import { HEROES } from './mock-heroes';
   providedIn: 'root'
 })
 export class HeroService {
+  constructor(private messageService: MessageService) { }
+  
   getHeroes():Observable<Hero[]> {
    const heroes = of(HEROES);
    this.messageService.add('HeroService: fetched heroes');
    return heroes;
   }
-  constructor(private messageService: MessageService) { }
+
+  getHero(id: number): Observable<Hero> {
+  const hero = HEROES.find(h => h.id === id)!;
+  this.messageService.add(`HeroService: fetched hero id=${id}`);
+  return of(hero);
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,20 +9,9 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent implements OnInit {
-  
-  
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-    
-  }
+export class HeroesComponent implements OnInit {  
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
